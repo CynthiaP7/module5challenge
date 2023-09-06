ffb7f2a08ca76f8ec68246fb4d42a57f3783d0fc
Cynthia P <cynthiads417@gmail.com>
2023-09-05 21:06:10 -0400

    fixed the color for past, present anf future in time blocks

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var saveButtons = $('.saveBtn');
   var taskForTheDay = $('.description');
   var currentHour = dayjs().hour();
-
+  console.log ("currentHour= ", currentHour);
   //Function to save to local storage
 
-  function saveTask (event) {
-    event.preventDefault ();
+  function saveTask(event) {
+    event.preventDefault();
     var hourId = $(this).parent().attr('id');
     var task = $(this).siblings('.description').val();
 
@@ -17,36 +17,37 @@ $(document).ready(function() {
   //add event listener to save button
   saveButtons.on('click', saveTask);
 
-  //claases for time blocks
-  $('.time-block').each(function (){
+  //classes for time blocks
+  $('.time-block').each(function () {
     var hourId = parseInt($(this).attr('id').split('-')[1]);
+    console.log  ("hourId= ", hourId);
     if (hourId < currentHour) {
       $(this).removeClass('present future').addClass('past');
-    } else if (hour === currentHour) {
+    } else if (hourId === currentHour) {
       $(this).addClass('present').removeClass('past future');
     } else {
       $(this).addClass('future').removeClass('past present');
     }
   });
 
-  var labelTime = dayjs().hour(hourId).format ('hA');
-  $(this).find('.hour').text(timeLabel);
+  // var labelTime = dayjs().hour(hourId).format ('hA');
+  // $(this).find('.hour').text(timeLabel);
 
 });
 
-  //display current date
+//display current date
 
-  $('#currentDay').text(dayjs().format('MMMM DD, YYYY'));
+$('#currentDay').text(dayjs().format('MMMM DD, YYYY'));
 
-  $('.time-block').each(function () {
-    var hourId = $(this).attr('id');
-    var savedTask = localStorage.getItem(hourId);
+$('.time-block').each(function () {
+  var hourId = $(this).attr('id');
+  var savedTask = localStorage.getItem(hourId);
 
-    if (savedTask) {
-      $(this).find('.description').val(savedTask);
-    }
+  if (savedTask) {
+    $(this).find('.description').val(savedTask);
+  }
 
-  });
+});
 
 
 
